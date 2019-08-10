66d57d5c37d9386c77afd8282c0835a68e8171d7
MAYTESI <mayte16inga@gmail.com>
2019-08-10 16:31:11 -0500

    subiendo cambios en test de orders.spec.js

--- controllers/orders.js
+++ controllers/orders.js
@@ -38,7 +38,6 @@ module.exports.postOrders = async(req, resp, next) => {
             return x !== null || undefined
         })
     }
-    console.log(arrOfProducts)
     const productsReales = req.body.products.map((p, index) => ({
         product: {
             id: mongodb.ObjectId(p.product),
@@ -57,21 +56,18 @@ module.exports.putOrders = async(req, resp, next) => {
         if (!req.body.status) {
             return next(400);
         }
-        const orderFindOne =await order.findOne({ _id: req.params.orderid });
+        const orderFindOne = await order.findOne({ _id: req.params.orderid });
         const item = {
             status: req.body.status || orderFindOne.status,
             userId: req.body.userId || orderFindOne.userId,
             client: req.body.client || orderFindOne.clientc
         };
-        const orderSaved=await orderFindOne.findOneAndUpdate({ _id: req.params.orderid }, { $set: item }, {runValidators: true, new: true })//,(err,order)=>{
-            if (orderSaved.status === 'canceled' || !orderSaved) {
+        const orderSaved = await order.findOneAndUpdate({ _id: req.params.orderid }, { $set: item }, { runValidators: true, new: true }) //,(err,order)=>{
+        if (orderSaved.status === 'canceled' || !orderSaved) {
             return next(404);
         };
         resp.send(orderSaved);
-   // });
-
     } catch (e) {
-        console.error(e)
         if (e.kind !== 'enum' && e.kind) {
             return next(404);
         }
@@ -80,7 +76,6 @@ module.exports.putOrders = async(req, resp, next) => {
 }
 
 module.exports.deleteOrders = (req, resp, next) => {
-
     order.findByIdAndRemove(req.params.orderid, (err, product) => {
         if (err) {
             return next(404)
